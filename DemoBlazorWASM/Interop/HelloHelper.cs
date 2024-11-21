c49350984e8d063c53c657486db2fdfafd179a50
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-11-21 10:20:06 +0100

    jsInterop

---
+++ DemoBlazorWASM/Interop/HelloHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+
+namespace DemoBlazorWASM.Interop;
+
+public class HelloHelper
+{
+    public string? Name { get; set; }
+
+    public HelloHelper(string name)
+    {
+        Name = name;
+    }
+
+    [JSInvokable]
+    public string SayHello()
+    {
+        return $"Hello, {Name}!";
+    }
+
+}
