fbd098bd729e643f9102801a2ea86e26ed84ec80
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-11-21 09:59:42 +0100

    javaScript

---
+++ DemoBlazorWASM/Interop/MioInteropJavaScript.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+
+namespace DemoBlazorWASM.Interop;
+
+public class MioInteropJavaScript
+{
+    private readonly IJSRuntime jSRuntime;
+
+    public MioInteropJavaScript(IJSRuntime jSRuntime)
+    {
+        this.jSRuntime = jSRuntime;
+    }
+
+    public async Task<int> SommaDaJavaScript(int a, int b)
+    {
+        return await jSRuntime.InvokeAsync<int>("miaSomma", a, b);
+    }
+
+    public async Task MiaMoltiplicazione(int a , int b)
+    {
+        await jSRuntime.InvokeVoidAsync("miaMoltiplicazione", a, b);
+    }
+
+}
---
+++ DemoBlazorWASM/wwwroot/mieFunzioni.js
@@ -0,0 +1,8 @@
+﻿window.miaSomma = function (a, b) {
+    return a + b;
+}
+
+window.miaMoltiplicazione =  (a, b) => {
+    const x = a * b;
+    console.log(x);
+}
